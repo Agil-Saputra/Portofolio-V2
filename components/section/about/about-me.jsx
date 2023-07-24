89cc581c67ae6a40b6844bf7398d16e137c53b34
Agil <88566690+Agil-Saputra@users.noreply.github.com>
2023-07-24 21:48:17 +0700

    fix lint

--- components/section/about/about-me.jsx
+++ components/section/about/about-me.jsx
@@ -5,12 +5,12 @@ const AboutMe = () => {
   return (
     <Container title="📝 About Me">
       <p className="paragraph-light">
-      Hello, I'm Agil Saputra, a Front End Developer with 1+ years of experience
+      Hello, I&lsquo;m Agil Saputra, a Front End Developer with 1+ years of experience
       in creating engaging and user-friendly websites. My technical skills
       include deep knowledge and experience in the frontend field, I also
       possess strong soft skills, such as effective teamwork, communication, and
       problem-solving abilities. I am passionate about bringing ideas to life
-      and creating digital experiences that make a difference in people's lives.
+      and creating digital experiences that make a difference in people&lsquo;s lives.
       </p>
     </Container>
   );
